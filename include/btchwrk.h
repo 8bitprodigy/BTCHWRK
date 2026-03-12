d1adf5c73fa40d20742ac4ddc0a734c47f16f4ad
8bitprodigy <chrisxdeboy@gmail.com>
2026-03-12 00:16:34 -0700

    Forgot to update `btchwrk.h`

--- include/btchwrk.h
+++ include/btchwrk.h
@@ -3,7 +3,13 @@
 
 
 #include "common.h"
+#include "dynamicarray.h"
+#include "hashfuncs.h"
+#include "hashmap.h"
+#include "hashset.h"
+#include "linkedlist.h"
 #include "sequence.h"
+#include "symbol.h"
 #include "text.h"
 
 
